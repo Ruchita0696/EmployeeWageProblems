54ee46175bc0c5ab4639c02bed53516f0a96fe09
ruchi06 <ruchi06@gmail.com>
2023-01-11 22:47:54 +0530

    UC9..EmployeetotalCompany

--- EmployeeWageProblems/Program.cs
+++ EmployeeWageProblems/Program.cs
@@ -2,46 +2,21 @@
 {
     public class Program
     {
-        public const int IS_FULL_TIME = 1, IS_PART_TIME = 2;
-
-        public static int computeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
-        {
-            // Variables
-            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-            // Comutation
-            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
-            {
-                totalWorkingDays++;
-                Random random = new Random();
-                int empCheck = random.Next(0, 3);
-                switch (empCheck)
-                {
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-                totalEmpHrs += empHrs;
-                Console.WriteLine("Days : " + totalWorkingDays + "Emp Hrs : " + empHrs);
-            }
-
-            int totalempwage = totalEmpHrs * empRatePerHour;
-            Console.WriteLine("Total Emp Wage of " + company + " is :" + totalempwage);
-            return totalempwage;
-        }
         static void Main(string[] args)
         {
-            computeEmpWage("Airtel", 30, 30, 100);
-            computeEmpWage("TCS", 40, 30, 80);
-            computeEmpWage("Wipro", 25, 25, 120);
-            computeEmpWage("Cipla", 23, 28, 111);
+            EmpEageBuilderObject Airtel = new EmpEageBuilderObject("Airtel", 30, 30, 100);
+            EmpEageBuilderObject TCS = new EmpEageBuilderObject("TCS", 40, 30, 80);
+            EmpEageBuilderObject Wipro = new EmpEageBuilderObject("Wipro", 25, 25, 120);
+            EmpEageBuilderObject Cipla = new EmpEageBuilderObject("Cipla", 23, 28, 111);
+            Airtel.computeEmpWage();
+            Console.WriteLine(Airtel.toString());
+            TCS.computeEmpWage();
+            Console.WriteLine(TCS.toString());
+            Wipro.computeEmpWage();
+            Console.WriteLine(Wipro.toString());
+            Cipla.computeEmpWage();
+            Console.WriteLine(Cipla.toString());
         }
     }
-
 }
 
