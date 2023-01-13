e59ed48bfa93747bafc5fb99a25aecfb0856cc67
ruchi06 <ruchi06@gmail.com>
2023-01-13 22:32:46 +0530

    Employee Using Interface

--- EmployeeWageProblems/Program.cs
+++ EmployeeWageProblems/Program.cs
@@ -4,18 +4,12 @@
     {
         static void Main(string[] args)
         {
-            EmpEageBuilderObject Airtel = new EmpEageBuilderObject("Airtel", 30, 30, 100);
-            EmpEageBuilderObject TCS = new EmpEageBuilderObject("TCS", 40, 30, 80);
-            EmpEageBuilderObject Wipro = new EmpEageBuilderObject("Wipro", 25, 25, 120);
-            EmpEageBuilderObject Cipla = new EmpEageBuilderObject("Cipla", 23, 28, 111);
-            Airtel.computeEmpWage();
-            Console.WriteLine(Airtel.toString());
-            TCS.computeEmpWage();
-            Console.WriteLine(TCS.toString());
-            Wipro.computeEmpWage();
-            Console.WriteLine(Wipro.toString());
-            Cipla.computeEmpWage();
-            Console.WriteLine(Cipla.toString());
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addcompanyEmpWage("Airtel", 30, 30, 100);
+            empWageBuilder.addcompanyEmpWage("TCS", 40, 30, 80);
+            empWageBuilder.addcompanyEmpWage("Wipro", 25, 25, 120);
+            empWageBuilder.addcompanyEmpWage("Cipla", 23, 28, 111);
+            empWageBuilder.computeEmpWage();
         }
     }
 }
